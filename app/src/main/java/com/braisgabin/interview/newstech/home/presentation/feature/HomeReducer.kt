e0cc438a7f26a9302a23c98a7a59e9fb0ca0fb49
Brais Gabín <braisgabin@gmail.com>
2019-11-11 12:56:21 +0100

    Implement Reducer

---
+++ app/src/main/java/com/braisgabin/interview/newstech/home/presentation/feature/HomeReducer.kt
@@ -0,0 +1,22 @@
+package com.braisgabin.interview.newstech.home.presentation.feature
+
+import com.badoo.mvicore.element.Reducer
+
+class HomeReducer : Reducer<State, Effect> {
+  override fun invoke(state: State, effect: Effect): State {
+    return when (effect) {
+      Effect.Error -> State.Error
+      Effect.Loading -> if (state is State.Data) {
+        state.copy(refreshing = true)
+      } else {
+        State.Loading
+      }
+      Effect.NoMoreItems -> if (state is State.Data) {
+        state.copy(loading = false)
+      } else {
+        state
+      }
+      is Effect.Data -> State.Data(effect.photos, loading = true, refreshing = false)
+    }
+  }
+}
